b806a272070041dfad27526012c9d3e14522224c
Dinh2468 <ngocdinht62204@dinht62204gmail.com>
2026-03-28 01:45:11 +0700

    fix timetam

--- backend/app/Models/Classroom.php
+++ backend/app/Models/Classroom.php
@@ -8,6 +8,7 @@ class Classroom extends Model
 {
     protected $table = 'classes';
     protected $fillable = ['class_name'];
+    public $timestamps = false;
     public function students()
     {
         return $this->hasMany(Student::class, 'class_id');
--- backend/app/Models/Student.php
+++ backend/app/Models/Student.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    protected $table = 'students'; // Đảm bảo đúng tên bảng
     protected $fillable = ['student_code', 'full_name', 'email', 'class_id'];
+
+    // Thêm dòng này để tránh lỗi sập web do thiếu timestamps
+    public $timestamps = false;
     public function classroom()
     {
         return $this->belongsTo(Classroom::class, 'class_id');
